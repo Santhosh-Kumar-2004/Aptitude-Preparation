2d582e2d6cf60a917031d7b7127b94feac49e4c8
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-07-10 20:40:21 +0530

    Day2 started

---
+++ Quantitative Aptitude/LCM_HCF/1.HCF(GCD).py
@@ -0,0 +1,8 @@
+# HCF (Highest Common Factor) or GCD (Greatest Common Divisor) of two numbers is the largest number that divides both numbers exactly.
+# | Numbers | HCF (GCD) | Explanation                                   |
+# | ------- | --------- | --------------------------------------------- |
+# | 12, 18  | 6         | Common factors: 1, 2, 3, 6                    |
+# | 8, 14   | 2         | Common factors: 1, 2                          |
+# | 7, 13   | 1         | No common factor except 1 (they are co-prime) |
+
+

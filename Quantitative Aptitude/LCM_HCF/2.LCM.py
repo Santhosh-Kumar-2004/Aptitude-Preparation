6b57ff8d0ebd7a153b969e87ee71ebb8d16032d4
Santhosh kumar <160613940+Santhosh-Kumar-2004@users.noreply.github.com>
2025-07-10 21:37:41 +0530

    LCM | smallest multiple of two numbers

---
+++ Quantitative Aptitude/LCM_HCF/2.LCM.py
@@ -0,0 +1,19 @@
+# 🔶 Definition:
+# The LCM (Least Common Multiple) of two numbers is the smallest positive number that is divisible by both.
+
+# METHOD-1
+# 🔸 Example:
+# Find LCM of 4 and 6:
+    # Multiples of 4 → 4, 8, 12, 16, 20, 24, ...
+    # Multiples of 6 → 6, 12, 18, 24, ...
+    # 🔁 First common multiple = 12
+    # 👉 LCM(4, 6) = 12
+
+
+# Formulae:
+    # LCM(a, b) = (a*b)/(GCD(a,b))
+
+# 🧮 Example:
+# Let a = 12, b = 18
+    # GCD(12, 18) = 6
+    # LCM = (12 × 18) ÷ 6 = 216 ÷ 6 = 36
